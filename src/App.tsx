1a6f29e8f0f7408d523184c0872828a5095a0c15
TheHesoyam <reviakindi@gmail.com>
2023-03-08 12:03:45 +0200

    clear app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import { OwnedNft, OwnedNftsResponse } from "alchemy-sdk";
-import React, { useEffect } from "react";
+import { OwnedNft } from "alchemy-sdk";
+import React from "react";
 import { useQuery } from "react-query";
 import alchemy from "./alchemy";
 import { Input, Card, Button } from './components'
 
-const address = '0x0AC2185374664768Ac7C44f9674a3c82ab31Ce67';
+const a = '0x0AC2185374664768Ac7C44f9674a3c82ab31Ce67';
 const PAGE_SIZE = 20;
 function App() {
-  const [address, setAddress] = React.useState('');
+  const [address, setAddress] = React.useState(a);
   const [nftList, setNftList] = React.useState<OwnedNft[]>([]);
-  const [total, setTotal] = React.useState<null | number>(null);
   const [pageKey, setPageKey] = React.useState<null | string>(null);
 
-  const { data, isLoading, error, refetch } = useQuery(
+  const { refetch } = useQuery(
     ['ownedNftList', address],
     () => {
       return alchemy.nft.getNftsForOwner(address, { ...(pageKey && { pageKey }), pageSize: PAGE_SIZE });
@@ -21,7 +20,6 @@ function App() {
       enabled: !!address,
       onSuccess: data => {
         setNftList(prevList => [...prevList, ...data.ownedNfts]);
-        setTotal(data.totalCount);
         setPageKey(data.pageKey ?? null);
       }
     }
@@ -32,8 +30,8 @@ function App() {
       <Input value={address} onChange={e => setAddress(e.target.value)} />
       <div>
         {nftList.map(nft => <Card {...nft} key={nft.tokenId} />)}
-        {pageKey && <Button onClick={() => refetch()}>load more</Button>}
       </div>
+      {pageKey && <Button onClick={() => refetch()}>load more</Button>}
     </div>
   );
 }
