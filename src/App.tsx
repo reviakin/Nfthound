9bc1578de5ba67ba3b084899c4b2b6e0a0d94619
TheHesoyam <reviakindi@gmail.com>
2023-03-08 09:13:04 +0200

    add load more

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,29 @@ import { OwnedNft, OwnedNftsResponse } from "alchemy-sdk";
 import React, { useEffect } from "react";
 import { useQuery } from "react-query";
 import alchemy from "./alchemy";
-import { Input, Card } from './components'
+import { Input, Card, Button } from './components'
 
+const address = '0x0AC2185374664768Ac7C44f9674a3c82ab31Ce67';
+const PAGE_SIZE = 20;
 function App() {
   const [address, setAddress] = React.useState('');
   const [nftList, setNftList] = React.useState<OwnedNft[]>([]);
+  const [total, setTotal] = React.useState<null | number>(null);
+  const [pageKey, setPageKey] = React.useState<null | string>(null);
 
-  const { data, isLoading, error } = useQuery(
+  const { data, isLoading, error, refetch } = useQuery(
     ['ownedNftList', address],
-    () => alchemy.nft.getNftsForOwner(address),
-    { enabled: !!address, onSuccess: data => setNftList(data.ownedNfts) }
+    () => {
+      return alchemy.nft.getNftsForOwner(address, { ...(pageKey && { pageKey }), pageSize: PAGE_SIZE });
+    },
+    {
+      enabled: !!address,
+      onSuccess: data => {
+        setNftList(prevList => [...prevList, ...data.ownedNfts]);
+        setTotal(data.totalCount);
+        setPageKey(data.pageKey ?? null);
+      }
+    }
   )
 
   return (
@@ -19,6 +32,7 @@ function App() {
       <Input value={address} onChange={e => setAddress(e.target.value)} />
       <div>
         {nftList.map(nft => <Card {...nft} key={nft.tokenId} />)}
+        {pageKey && <Button onClick={() => refetch()}>load more</Button>}
       </div>
     </div>
   );
--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 type TProps = {
   children: React.ReactNode;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>
 }
 
-const Button = React.memo<TProps>(({ children }) => {
-  return <button>{children}</button>
+const Button = React.memo<TProps>(({ children, onClick }) => {
+  return <button onClick={onClick}>{children}</button>
 })
 
 export default Button;
