bad5b4e49b7023c1fd9f62f2986f6fbfaef7f342
TheHesoyam <reviakindi@gmail.com>
2023-03-08 09:20:03 +0200

    add todo

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -4,6 +4,7 @@ import React from "react";
 type TProps = OwnedNft;
 const Card = React.memo<TProps>(({ title, media }) => {
 
+  // TODO handle if type isn't png
   const imageSrc = media.find(mediaItem => mediaItem.thumbnail)?.thumbnail;
 
   return <div><h2>{title}</h2>
